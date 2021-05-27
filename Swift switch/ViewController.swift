d0d645d3a702ab5c3d6ade92caa81e77c256d578
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-05-27 17:22:28 +0200

    adding background colour

--- Swift switch/ViewController.swift
+++ Swift switch/ViewController.swift
@@ -18,10 +18,12 @@ class ViewController: UIViewController {
     @IBAction func switchPressed(_ sender: UISwitch) {
         if `switch`.isOn{
             lbl.text = "Switch is on"
+            self.view.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
             
         }
         else{
             lbl.text = "Switch is off"
+            self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
     
